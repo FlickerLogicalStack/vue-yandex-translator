0a17eb4a0aaf5ac7e82982208866287f5c649fbe
LogicalStack <9medved@mail.ru>
2019-01-27 01:09:38 +0300

    Shiiiiiiiiiiiiiiiiit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        quotes: [
+            'error',
+            'single'
+        ],
+        indent: [
+            'warn',
+            4
+        ],
+        semi: [
+            'warn',
+            'always'
+        ]
+    }
+};
